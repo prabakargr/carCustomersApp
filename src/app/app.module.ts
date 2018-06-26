7f986dcf01d4bfa918495e19270f211d96d2cfc3
DESKKIRAJ <esakkiraj6394@gmail.com>
2018-06-26 18:11:57 +0530

    login page update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { TrackingComponent } from './tracking/tracking.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    TrackingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    HttpModule,
+    FormsModule, ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'login' },
+      { path: 'login', component: LoginComponent },
+      { path: 'navbar', component: NavbarComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'track', component: TrackingComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor( private router : Router ) { }
+
+  ngOnInit() {
+  }
+
+    onLogin(value){      
+        console.log(value); 
+        this.router.navigate(['/home']); 
+
+      }
+
+}
